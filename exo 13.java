76a8788dc72e6f2eb9dcd4287d351a965289cec5
YassineElazzati <yassine.elazzati@outlook.fr>
2023-08-14 19:29:35 +0200

    feat: exo 13

---
+++ exo 13.java
@@ -0,0 +1,16 @@
+public class Positive {
+
+    public static int sum(int[] arr) {
+        int a = 0;
+        for (int num : arr) {
+            if (num > 0) {
+                a += num;
+            }
+        }
+        return a;
+    }
+}
+
+
+https://www.codewars.com/kata/5715eaedb436cf5606000381/train/java
+
