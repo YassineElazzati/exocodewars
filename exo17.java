d7c45738f63c9ed5c250ad37da377be624a64301
YassineElazzati <yassine.elazzati@outlook.fr>
2023-08-31 20:49:57 +0200

    exo 17

---
+++ exo17.java
@@ -0,0 +1,14 @@
+public class Number {
+
+    public boolean isEven(double n) {
+        // Your awesome code here!
+      if(n%2==0){
+        return true;
+      }else{
+        return false;
+      }
+      
+    }
+  }
+
+//  even or not 
